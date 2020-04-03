8aa4bb017614957e991a1e2ed2e4af540b583439
daomengRen <46952617+daomengRen@users.noreply.github.com>
2020-04-03 18:23:48 +0800

    Update hostloc_get_points.py

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -10,7 +10,7 @@ def randomly_gen_uspace_url():
     url_list = []
     # 访问小黑屋用户空间不会获得积分、生成的随机数可能会重复，这里多生成两个链接用作冗余
     for i in range(12):
-        uid = random.randint(310000, 319000)
+        uid = random.randint(310000, 315000)
         url = "https://ssdforum.org/space-uid-{}.html".format(str(uid))
         url_list.append(url)
     return url_list
